600d0992768beef5da4fced660a49949fe078987
Todd Farr <todd.farr@gmail.com>
2015-07-02 11:29:57 +0900

    Clean up todo mark completion view

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,7 +1,11 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
 
-  #combining validations for presence & length 
+  #combining validations for presence & length
   validates :content, presence: true,
                       length: { minimum: 2 }
+
+  def completed?
+    !completed_at.blank?
+  end
 end
